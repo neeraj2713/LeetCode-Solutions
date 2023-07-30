d4eabe1229a8f8194722be0a952716119c9e960e
Neeraj Shekhawat <neeraj.shekhawat2003@gmail.com>
2023-07-30 13:23:10 +0530

    Time: 6 ms (84.09%), Space: 7.3 MB (79.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        if(s.size() != t.size()) return false;
+        map<char, int> bucket1;
+        map<char, int> bucket2;
 
-        return s==t;
+        for(char c:s){
+            bucket1[c]++;
+        }
+        
+        for(char c:t){
+            bucket2[c]++;
+        }
+
+        return bucket1==bucket2;
+        
     }
 };
