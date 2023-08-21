75251c6f545f5d486e799957de76862874bb0e76
Neeraj Shekhawat <neeraj.shekhawat2003@gmail.com>
2023-08-21 15:29:04 +0530

    Time: 8 ms (9.38%), Space: 10.5 MB (5.69%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        int pos = s.find(part);
+    void removeOcc(string& s, string part){
+        int found = s.find(part);
+        if(found != string::npos){
+            string left_part = s.substr(0, found);
+            string right_part = s.substr(found+part.size(), s.length());
+            s = left_part+right_part;
 
-        while(pos != string::npos){
-            s.erase(pos, part.length());
-            pos = s.find(part);
+            removeOcc(s, part);
+        } else {
+            return;
         }
+    }
 
+    string removeOccurrences(string s, string part) {
+        removeOcc(s, part);
         return s;
     }
 };
