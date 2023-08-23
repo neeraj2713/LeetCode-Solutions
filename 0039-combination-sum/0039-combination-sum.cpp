77ddb6579cb5af73e6c87e21fbd6059eb578ed8e
Neeraj Shekhawat <neeraj.shekhawat2003@gmail.com>
2023-08-23 17:45:57 +0530

    Time: 4 ms (80.08%), Space: 10.7 MB (85.66%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        backtrack(candidates, 0, target, output, ans);
+        return ans;
+    }
+
+    void backtrack(vector<int>& candidates, int i, int target, vector<int>& output, vector<vector<int>>& ans){
+        if(i >= candidates.size()){
+            if(target == 0){
+                ans.push_back(output);
+            }
+            return;
+        }
+
+        if(candidates[i] <= target){
+            output.push_back(candidates[i]);
+            backtrack(candidates, i, target-candidates[i], output, ans);
+            output.pop_back();
+        }
+
+        backtrack(candidates, i+1, target, output, ans);
+    }
+};
