b83a98c97a4b8c0bb5f070553515441d7fc09142
Neeraj Shekhawat <neeraj.shekhawat2003@gmail.com>
2023-08-23 18:51:38 +0530

    Time: 0 ms (100%), Space: 6 MB (27.87%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0) return 1;
+        if(n == 1) return x;
+
+        if(n<0){
+            x = 1/x;
+            n = abs(n);
+        }
+
+        double temp = myPow(x, n/2);
+        if(n%2 == 0){
+            return temp*temp;
+        } else {
+            return temp*temp*x;
+        }
+    }
+};
