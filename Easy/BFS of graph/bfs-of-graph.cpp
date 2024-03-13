4bc99054da985d907ce4c1edba297e87d6bedf99
Neeraj Singh Shekhawat <neeraj.shekhawat2003@gmail.com>
2024-03-13 21:34:55 +0530

    Updated Solution - GfG to GitHub

--- Easy/BFS of graph/bfs-of-graph.cpp
+++ Easy/BFS of graph/bfs-of-graph.cpp
@@ -9,18 +9,17 @@ class Solution {
     vector<int> bfsOfGraph(int V, vector<int> adj[]) {
         // Code here
         int vis[V] = {0};
-        vector<int> bfs;
         vis[0] = 1;
         queue<int> q;
         q.push(0);
+        vector<int> bfs;
         
         while(!q.empty()){
             int node = q.front();
             q.pop();
             bfs.push_back(node);
-            
             for(auto it : adj[node]){
-                if(!vis[it] == 1){
+                if(!vis[it]){
                     vis[it] = 1;
                     q.push(it);
                 }
