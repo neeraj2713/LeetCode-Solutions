dc251e26ab5862985e0a4635828ae154b9d64b3c
Neeraj Singh Shekhawat <neeraj.shekhawat2003@gmail.com>
2024-04-14 17:45:22 +0530

    Time: 21 ms (80.54%) | Memory: 16.1 MB (68.53%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-    int solveRec(vector<int>& coins, int amount, vector<int>& dp){
-        if(amount == 0) return 0;
-        if(amount < 0) return INT_MAX;
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1, INT_MAX);
+        dp[0] = 0;
         
-        if(dp[amount] != -1) return dp[amount];
-        
-        int mini = INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            int ans = solveRec(coins, amount - coins[i], dp);
-            if(ans != INT_MAX){
-                mini = min(mini, ans+1);
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.size();j++){
+                if(i-coins[j] >= 0 && dp[i-coins[j]] != INT_MAX){
+                    dp[i] = min(dp[i], 1 + dp[i-coins[j]]);
+                }
             }
         }
         
-        dp[amount] = mini;
-        
-        return mini;
-    }
-    
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1, -1);
-        int ans = solveRec(coins, amount, dp);
-        return ans == INT_MAX ? -1 : ans;
+        if(dp[amount] == INT_MAX) return -1;
+        return dp[amount];
     }
 };
