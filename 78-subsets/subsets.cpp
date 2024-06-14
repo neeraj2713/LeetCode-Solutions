e038529bf7cb221a5ce99a906b1c237bc7a82cec
Neeraj Singh Shekhawat <neeraj.shekhawat2003@gmail.com>
2024-06-14 09:59:18 +0530

    Time: 4 ms (37.84%) | Memory: 8.2 MB (77.86%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,22 +1,21 @@
 class Solution {
 private:
-    void solver(vector<int>& nums, int index, vector<int>& output, vector<vector<int>>& ans){
+    void backtrack(vector<int>& nums, int index, vector<int>& output, vector<vector<int>>& ans){
         if(index >= nums.size()){
             ans.push_back(output);
             return;
         }
 
         output.push_back(nums[index]);
-        solver(nums, index+1, output, ans);
+        backtrack(nums, index+1, output, ans);
         output.pop_back();
-        solver(nums, index+1, output, ans);
+        backtrack(nums, index+1, output, ans);
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> output;
-        int index = 0;
-        solver(nums, index, output, ans);
+        backtrack(nums, 0, output, ans);
         return ans;
     }
 };
