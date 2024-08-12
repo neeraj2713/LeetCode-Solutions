9c80eaaa5e7a145a9dee16657b2b7f98d98b8139
Neeraj Singh Shekhawat <neeraj.shekhawat2003@gmail.com>
2024-08-12 11:42:52 +0530

    Time: 15 ms (93.64%) | Memory: 20.9 MB (42.71%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int l = 0;
+        int r = 0;
+
+        while(l < s.size() && r<g.size()){
+            if(g[r] <= s[l] ){
+                r++;
+            }
+            l++;
+        }
+
+        return r;
+    }
+};
