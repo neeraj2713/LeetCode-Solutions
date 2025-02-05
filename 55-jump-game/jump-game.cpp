5faf84199c4e8b47a5a371d32e3c1267c6e993f9
Neeraj Singh Shekhawat <neeraj.shekhawat2003@gmail.com>
2025-02-06 00:03:09 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (60.94%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -4,10 +4,10 @@ public:
         int n = nums.size();
         int lastGood = n-1;
 
-        for(int i=n-2;i>=0;i--){
-            if(nums[i] + i >= lastGood){
+        for(int i=n-1;i>=0;i--){
+            if(nums[i] + i >= lastGood ){
                 lastGood = i;
-            } 
+            }
         }
 
         return lastGood == 0;
