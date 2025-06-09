9a658a5ad60c19ab60a265bf0c06f17e03ede94e
Neeraj Singh Shekhawat <neeraj.shekhawat2003@gmail.com>
2025-06-09 09:43:17 +0530

    Time: 24 ms (6.33%) | Memory: 23 MB (12.57%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,21 +1,23 @@
 class Solution {
 private:
-    bool countHours(vector<int>& piles, int mid, int h){
-        long long totalHours = 0;
+    bool findHours(vector<int>& piles, int h, int k){
+        int totalHours = 0;
         for(int i=0;i<piles.size();i++){
-            totalHours += (ceil((double)piles[i]/(double)mid));
+            totalHours += ceil((double)(piles[i]) / (double)(k));
+            if(totalHours > h) return false;
         }
-        return totalHours <= h ? true : false;
+
+        return true;
     }
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int ans = 0;
         int s = 1;
         int e = *max_element(piles.begin(), piles.end());
+        int ans = 0;
 
         while(s<=e){
             int mid = s+(e-s)/2;
-            if(countHours(piles, mid, h)){
+            if(findHours(piles, h, mid)) {
                 ans = mid;
                 e = mid-1;
             } else {
