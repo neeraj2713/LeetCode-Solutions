3477e1617ccc77464e417308fd644d6223935100
Neeraj Singh Shekhawat <neeraj.shekhawat2003@gmail.com>
2025-06-16 19:36:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (5.40%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
+        stack<int> st;
 
-        for(int i=0;i<s.size();i++){
-            char ch = s[i];
-            if(ch == '(' || ch == '[' || ch == '{'){
-                st.push(ch);
-            } else if(ch == ')' && !st.empty() && st.top() == '(' ){
+        for(char c : s){
+            if(c == ')' && !st.empty() && st.top() == '(') {
                 st.pop();
-            } else if(ch == ']' && !st.empty() && st.top() == '[' ) {
+            } else if(c == '}' && !st.empty() && st.top() == '{') {
                 st.pop();
-            } else if(ch == '}'  && !st.empty() && st.top() == '{') {
+            } else if(c == ']' && !st.empty() && st.top() == '[') {
                 st.pop();
             } else {
-                return false;
+                st.push(c);
             }
         }
 
