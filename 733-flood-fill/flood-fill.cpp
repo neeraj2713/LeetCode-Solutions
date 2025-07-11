8bbae67107d730a385160eacc8c5041d1ff61acc
Neeraj Shekhawat <neeraj.shekhawat2003@gmail.com>
2025-07-11 09:10:09 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (61.40%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -5,12 +5,11 @@ private:
         int n = image.size();
         int m = image[0].size();
 
-        for(int i=0;i<4;i++){
+        for(int i=0;i<4;i++) {
             int nrow = row + delRow[i];
             int ncol = col + delCol[i];
-
-            if(nrow >= 0 && nrow < n && ncol >=0 && ncol < m &&
-            image[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor){
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&
+            image[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor) {
                 dfs(nrow, ncol, ans, image, newColor, delRow, delCol, iniColor);
             }
         }
@@ -21,7 +20,6 @@ public:
         vector<vector<int>> ans = image;
         int delRow[] = {-1, 0, 1, 0};
         int delCol[] = {0, 1, 0, -1};
-
         dfs(sr, sc, ans, image, color, delRow, delCol, iniColor);
         return ans;
     }
