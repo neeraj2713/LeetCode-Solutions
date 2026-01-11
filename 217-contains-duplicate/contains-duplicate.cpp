ea8dfa84454738e9af6055097f59102f15b4bed6
Neeraj Shekhawat <neeraj.shekhawat2003@gmail.com>
2026-01-12 00:23:35 +0530

    Time: 62 ms (21.66%) | Memory: 93 MB (13.37%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, int> mpp;
-        for(auto num : nums) mpp[num]++;
+        int n = nums.size();
 
-        for(auto v : mpp){
-            if(v.second > 1) return true;
+        unordered_map<int, int> mpp;
+        for(int i=0;i<n;i++) {
+            mpp[nums[i]]++;
+        }
+
+        for(auto &p: mpp){
+            if(p.second > 1) return true;
         }
 
         return false;
